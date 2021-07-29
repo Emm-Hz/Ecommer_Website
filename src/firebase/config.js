679a58abaf784a240ccf4bff3d028caa05eea96b
EmmJHZ <ej.higuer@gmail.com>
2021-07-29 19:36:29 -0400

    Add files via upload

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA9mVJIQb8UgCpZ-79a4qM7p0V-QuKSzVI",
+    authDomain: "rv-store.firebaseapp.com",
+    projectId: "rv-store",
+    storageBucket: "rv-store.appspot.com",
+    messagingSenderId: "887427212933",
+    appId: "1:887427212933:web:f688542911578cf1b9abec",
+    measurementId: "G-8MDD2GC3ER"
+  };
